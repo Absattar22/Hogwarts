e69a0fae237ca039bf95e716dc09812c15b3ab2d
zeyadabdelsattar <zeyadabdelsattar@gmail.com>
2024-09-05 03:00:33 +0300

    Add the app views

---
+++ lib/constants/strings.dart
@@ -0,0 +1,1 @@
+const baseUrl = "https://hp-api.onrender.com/api/";
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import network_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
 }
